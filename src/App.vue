f7e7bb5de61e279f45bfaeae115e39e98aa265ee
BarrioTropical <barriotropical@gmail.com>
2022-11-16 15:17:50 +0100

    search bar component
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
-<script setup>
-
-</script>
-
 <template>
-  
+    <HeaderComponent/>
+    <main>
+    <MainComponent/>
+    
+    </main>
 </template>
 
-<style scoped>
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+
+    export default {
+    components: { HeaderComponent, MainComponent }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@fortawesome/fontawesome-free/scss/fontawesome.scss";
+@import "@fortawesome/fontawesome-free/scss/solid.scss";
+@import "@fortawesome/fontawesome-free/scss/brands.scss";
+@import "@fortawesome/fontawesome-free/scss/regular.scss";
 
 </style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Sono Main
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
